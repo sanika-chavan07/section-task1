7d8e0bd81854ec8208301e86d18fa0cd1e9ba80a
sanika-chavan07 <sanikachavan4836@gmail.com>
2026-01-30 18:12:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+let cl=console.log
+let sidebar=document.getElementById("sidebar")
+let sideBarOpen=document.getElementById("sideBarOpen")
+let sideBarClose=document.getElementById("sideBarClose")
+
+function onsidebartoggle(){
+sidebar.classList.toggle('active')
+}
+
+sideBarOpen.addEventListener("click",onsidebartoggle)
+sideBarOpen.addEventListener("click",onsidebartoggle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cl = console.log;
+
+// const sideBarOpen = document.getElementById('sideBarOpen')
+// const sidebar = document.getElementById('sidebar')
+// const sideBarClose = document.getElementById('sideBarClose')
+// function onSideBarToggle(){
+//     sidebar.classList.toggle('active')
+// }
+
+// sideBarOpen.addEventListener("click",onSideBarToggle)
+// sideBarClose.addEventListener("click",onSideBarToggle)
+
+
+
